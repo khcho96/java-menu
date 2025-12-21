7533ff439d48be91e8ead33c8213f568ccb13d6b
khcho96 <khcho1492@gmail.com>
2025-12-21 14:34:34 +0900

    feat: 시작 메시지 출력

---
+++ src/main/java/menu/view/OutputView.java
@@ -0,0 +1,15 @@
+package menu.view;
+
+public class OutputView {
+
+    private static final String NEW_LINE = System.lineSeparator();
+    private static final String START_MESSAGE = "점심 메뉴 추천을 시작합니다.";
+
+    public static void printStart() {
+        System.out.println(START_MESSAGE);
+    }
+
+    public static void printErrorMessage(IllegalArgumentException e) {
+        System.out.println(e.getMessage());
+    }
+}
